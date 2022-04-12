8faaf22cfaa2758dcd04928b85700d6dd6c44195
spoon lava <lavaspoon@naver.com>
2022-04-13 08:16:40 +0900

    JDBC Connection 작업

---
+++ jdbc_study/src/main/java/hello/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package hello.connection;
+
+public abstract class ConnectionConst { //생성 못하게 abstract
+    public static final String URL = "jdbc:h2:tcp://localhost/~/test";
+    public static final String USERNAME = "sa";
+    public static  final String PASSWORD = "";
+}
