c658f37246ec425266350521f0caa44f0e78409d
spoon lava <lavaspoon@naver.com>
2022-04-22 07:45:20 +0900

    JDBC 개발 - 수정

--- jdbc_study/src/main/java/hello/repository/MemberRepositoryV0.java
+++ jdbc_study/src/main/java/hello/repository/MemberRepositoryV0.java
@@ -65,6 +65,27 @@ public class MemberRepositoryV0 {
         }
     }
 
+    public void update(String memberId, int money) throws SQLException {
+        String sql = "update member set money = ? where memeber_id=?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setInt(1, money);
+            pstmt.setString(2, memberId);
+            int resulutSize = pstmt.executeUpdate(); //영향받은 Row 숫자를 반환함(Int형)
+            log.info("resultSize = {} ", resulutSize);
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
     //pstmt에서 exeption에 생기는 경우 con은 close 되지 않기때문에 따로 함수 생성
     //Statement, PreparedStatement 차이점은 쿼리 변수를 바인딩 할 수 있고 없고 차이(상속받음)
     private void close(Connection con, Statement stmt, ResultSet rs){
