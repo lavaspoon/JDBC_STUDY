968ce944d522a439da287432fb8adfef8e83f54a
lavaspoon <lavaspoon@naver.com>
2022-04-22 12:23:47 +0900

    JDBC - 삭제 개발

--- jdbc_study/src/main/java/hello/repository/MemberRepositoryV0.java
+++ jdbc_study/src/main/java/hello/repository/MemberRepositoryV0.java
@@ -86,6 +86,26 @@ public class MemberRepositoryV0 {
         }
     }
 
+    public void delete(String memberId) throws SQLException {
+        String sql = "delete from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            con = getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            int resulutSize = pstmt.executeUpdate(); //영향받은 Row 숫자를 반환함(Int형)
+            log.info("resultSize = {} ", resulutSize);
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
     //pstmt에서 exeption에 생기는 경우 con은 close 되지 않기때문에 따로 함수 생성
     //Statement, PreparedStatement 차이점은 쿼리 변수를 바인딩 할 수 있고 없고 차이(상속받음)
     private void close(Connection con, Statement stmt, ResultSet rs){
