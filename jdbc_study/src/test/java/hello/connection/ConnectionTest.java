04078c0b8d66bce51bb9dcbd3f6207ebc57c3869
spoon lava <lavaspoon@naver.com>
2022-04-23 23:40:37 +0900

    JDBC 개발 - Hikari 테스트 개발

--- jdbc_study/src/test/java/hello/connection/ConnectionTest.java
+++ jdbc_study/src/test/java/hello/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
@@ -32,6 +33,20 @@ public class ConnectionTest {
         //DriverMangerDataSource - 항상 새로운 커넥션을 획득
         DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
         useDataSrouce(dataSource);
+    }
+
+    @Test
+    void dataSourceConnectionPool() throws SQLException, InterruptedException {
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSrouce(dataSource);
+        Thread.sleep(1000);
     }
 
     private void useDataSrouce(DataSource dataSource) throws SQLException {
