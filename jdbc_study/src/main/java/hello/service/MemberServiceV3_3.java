324095d7d82cbd0f0faedd880d1a767bf5febe33
spoon lava <lavaspoon@naver.com>
2022-07-03 15:24:03 +0900

    Transactional AOP 적용 시작

---
+++ jdbc_study/src/main/java/hello/service/MemberServiceV3_3.java
@@ -0,0 +1,59 @@
+package hello.service;
+
+import hello.domain.Member;
+import hello.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - @Transactional AOP
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV3_3 {
+
+//    private final PlatformTransactionManager transactionManager;
+    private final TransactionTemplate txTemplate;
+    private final MemberRepositoryV3 memberRepositoryV3;
+
+    public MemberServiceV3_3(PlatformTransactionManager transactionManager, MemberRepositoryV3 memberRepositoryV3) {
+        this.txTemplate = new TransactionTemplate(transactionManager);
+        this.memberRepositoryV3 = memberRepositoryV3;
+    }
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+
+        txTemplate.executeWithoutResult((status) -> {
+            try {
+                bizLogic(fromId, toId, money);
+            } catch (SQLException e) {
+                throw new IllegalStateException();
+            }
+        });
+
+        try{
+            bizLogic(fromId, toId, money); //비지니스 로직 수행\
+        } catch (Exception e) {
+            throw new IllegalStateException();
+        }
+    }
+
+    private void bizLogic(String fromId, String toId, int money) throws SQLException {
+        Member fromMember = memberRepositoryV3.findById(fromId);
+        Member toMember = memberRepositoryV3.findById(toId);
+
+        memberRepositoryV3.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepositoryV3.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if(toMember.getMemberId().equals("ex")) {
+            throw new IllegalStateException("이체중 예외 발생");
+        }
+    }
+}
